22bf09a95245c2c5c4d8de9e59c7ca1b1bf1546f
ahmadfaraz119 <ahmadfarazbrowntech119@gmail.com>
2023-03-27 14:55:38 +0500

    play_earn_banner

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -15,7 +15,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
-
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <!-- Css Styles -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}" type="text/css">
--- resources/views/user/game.blade.php
+++ resources/views/user/game.blade.php
@@ -1,204 +1,213 @@
 @extends('layout.main')
 
 @section('content')
-    <!-- Header End -->
+<!-- Header End -->
 
-    <!-- Hero Section Begin -->
-    <section class="hero">
-        <div class="row">
-                <div class="col-md-2">
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                         crossorigin="anonymous"></script>
-                    <!-- Sidebar ads -->
-                    <ins class="adsbygoogle"
-                         style="display:block"
-                         data-ad-client="ca-pub-7235363930426530"
-                         data-ad-slot="6477080102"
-                         data-ad-format="auto"
-                         data-full-width-responsive="true"></ins>
-                    <script>
-                         (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
-                <div class="col-md-8">
-                    <div class="container">
-                        <div class="hero__slider owl-carousel">
-                            <div class="hero__items set-bg" data-setbg="img/hero/background.jpg">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="hero__text">
-                                            <h2>Welcome to Play And Earn</h2>
-                                            <!-- <p>After 30 days of travel across the world...</p> -->
-                                            <a href="{{ url('playgame') }}"><span>Play Now</span> <i class="fa fa-angle-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                     crossorigin="anonymous"></script>
-                <!-- Sidebar ads -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-7235363930426530"
-                     data-ad-slot="6477080102"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                <script>
-                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>
+<!-- Hero Section Begin -->
+<section class="hero">
+    <div class="row">
+        <div class="col-md-2">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
+            <!-- Sidebar ads -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6477080102" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
         </div>
-    </section>
-
-
-        <!-- Product Section Begin -->
-
-
-
-    <!-- Hero Section Begin -->
-    <section class="hero">
-        <div class="row">
-                <div class="col-md-2">
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                         crossorigin="anonymous"></script>
-                    <!-- Sidebar ads -->
-                    <ins class="adsbygoogle"
-                         style="display:block"
-                         data-ad-client="ca-pub-7235363930426530"
-                         data-ad-slot="6477080102"
-                         data-ad-format="auto"
-                         data-full-width-responsive="true"></ins>
-                    <script>
-                         (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
-                <div class="col-md-8">
-                    <div class="container">
-                        <div class="hero__slider owl-carousel">
-                            <div class="hero__items set-bg" data-setbg="img/hero/background.jpg">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="hero__text">
-                                            <h2>Welcome to Play And Earn</h2>
-                                            <!-- <p>After 30 days of travel across the world...</p> -->
-                                            <a href="{{ url('playgame/pinball') }}"><span>Play Now</span> <i class="fa fa-angle-right"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                     crossorigin="anonymous"></script>
-                <!-- Sidebar ads -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-7235363930426530"
-                     data-ad-slot="6477080102"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                <script>
-                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>
-    </section>
-    <!--  -->
-
-
-    <!-- Hero Section Begin -->
-    <section class="hero">
-        <div class="row">
-            <div class="col-md-2">
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                        crossorigin="anonymous"></script>
-                <!-- Sidebar ads -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-7235363930426530"
-                     data-ad-slot="6477080102"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>
-            <div class="col-md-8">
-                <div class="container">
+        <div class="col-md-8">
+            <div class="container ">
+                <div class="banner">
                     <div class="hero__slider owl-carousel ">
-                        <div class="hero__items set-bg" data-setbg="{{asset('img/hero/background.jpg')}}">
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <div class="hero__text">
-                                        <h2>Welcome to Play And Earn</h2>
-                                        <!-- <p>After 30 days of travel across the world...</p> -->
-                                        <a href="{{ url('playgame/tank_carnage') }}"><span>Play Now</span> <i class="fa fa-angle-right"></i></a>
+                        <div class="banner_bg">
+                            <div class="hero__items set-bg" data-setbg="img/hero/background.jpg">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="hero__text">
+                                            <h2>Welcome to Play And Earn</h2>
+                                            <!-- <p>After 30 days of travel across the world...</p> -->
+                                            <a href="{{ url('playgame') }}"><span>Play Now</span> <i
+                                                    class="fa fa-angle-right"></i></a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-
-
+                    </div>
+                    <div class="game_bg">
+                        <div class="banner_icon">
+                        <i class="fa-sharp fa-solid fa-lock"></i>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-2">
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                        crossorigin="anonymous"></script>
-                <!-- Sidebar ads -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-7235363930426530"
-                     data-ad-slot="6477080102"
-                     data-ad-format="auto"
-                     data-full-width-responsive="true"></ins>
-                <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>
-    </section>
-    <!--  -->
-
-
-
-
-
-
-
-
-        <section class="product spad">
-        <div class="container">
-
         </div>
+        <div class="col-md-2">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
+            <!-- Sidebar ads -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6477080102" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+    </div>
+</section>
+
+
+<!-- Product Section Begin -->
+
+
+
+<!-- Hero Section Begin -->
+<section class="hero">
+    <div class="row">
+        <div class="col-md-2">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
+            <!-- Sidebar ads -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6477080102" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+        <div class="col-md-8">
+            <div class="container ">
+                <div class="banner">
+                    <div class="hero__slider owl-carousel ">
+                        <div class="banner_bg">
+                            <div class="hero__items set-bg" data-setbg="img/hero/background.jpg">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="hero__text">
+                                            <h2>Welcome to Play And Earn</h2>
+                                            <!-- <p>After 30 days of travel across the world...</p> -->
+                                            <a href="{{ url('playgame') }}"><span>Play Now</span> <i
+                                                    class="fa fa-angle-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="game_bg">
+                        <div class="banner_icon">
+                        <i class="fa-sharp fa-solid fa-lock"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
+            <!-- Sidebar ads -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6477080102" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+</section>
+<!--  -->
+
+
+<!-- Hero Section Begin -->
+<section class="hero">
+    <div class="row">
+        <div class="col-md-2">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
+            <!-- Sidebar ads -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6477080102" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+        <div class="col-md-8">
+            <div class="container ">
+                <div class="banner">
+                    <div class="hero__slider owl-carousel ">
+                        <div class="banner_bg">
+                            <div class="hero__items set-bg" data-setbg="img/hero/background.jpg">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="hero__text">
+                                            <h2>Welcome to Play And Earn</h2>
+                                            <!-- <p>After 30 days of travel across the world...</p> -->
+                                            <a href="{{ url('playgame') }}"><span>Play Now</span> <i
+                                                    class="fa fa-angle-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="game_bg">
+                        <div class="icon_parent">
+                            <div class="banner_icon">
+                            <i class="fa-sharp fa-solid fa-lock"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
+            <!-- Sidebar ads -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6477080102" data-ad-format="auto" data-full-width-responsive="true"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+</section>
+<!--  -->
+
+
+
+
+
+
+
+
+<section class="product spad">
+    <div class="container">
+
+    </div>
     </div>
     <section class="hero gg" style="height:420px;">
         <div class="container">
-           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
-                 crossorigin="anonymous"></script>
+            <script async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7235363930426530"
+                crossorigin="anonymous"></script>
             <!-- In feed -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-7235363930426530"
-                 data-ad-slot="6285508418"
-                 data-ad-format="auto"
-                 data-full-width-responsive="true"></ins>
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7235363930426530"
+                data-ad-slot="6285508418" data-ad-format="auto" data-full-width-responsive="true"></ins>
             <script>
-                 (adsbygoogle = window.adsbygoogle || []).push({});
+            (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
         </div>
     </section>
 
-</div>
-</div>
-</div>
-</div>
+    </div>
+    </div>
+    </div>
+    </div>
 </section>
-    @endsection
+@endsection
